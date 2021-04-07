b7e0eaa5886adb99202e102db285c28ad6c9f1f5
my110110 <446910086@qq.com>
2021-04-07 17:40:16 +0800

    '新建项目'

---
+++ src/main/java/cn/wft/newdev/NewDevApplication.java
@@ -0,0 +1,13 @@
+package cn.wft.newdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/wft/newdev/NewDevApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wft.newdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
